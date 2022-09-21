c6705233bed33eba95e03493ba07da57255d7bbf
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2022-09-21 01:04:36 -0500

    feat:entendiendo proceso hijos y de child procces viendo exec y spawn

---
+++ sonProcess.js
@@ -0,0 +1,30 @@
+//Puede ejecutar otro proceso en el sistema son los procesos hijos
+//traemos la librerias que queremos usar
+const { exec, spawn } = require('child_process');
+
+// directamente podemos ejecutar el comando de consola que queramos CON EXEC
+exec('node console.js', (err, stdout, sterr) => {
+    if (err) {
+        console.error(err);
+        return false;
+    }
+
+    console.log(stdout);
+})
+
+//Con spawn similar a exec pero más completo por que podemos observar
+// a detalle el proceso usado, extraer diferente tipode información de estos procesos
+let proces = spawn('dir');
+console.log(proces);
+console.log(proces.pid);
+console.log(proces.connected);
+
+proces.stdout.on('data', function(dato) {
+    console.log(proces.killed);
+    console.log(dato.toString());
+});
+proces.on('exit', function() {
+    console.log('el proceso termino');
+    console.log(proces.killed);
+
+})
