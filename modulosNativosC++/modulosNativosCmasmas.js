b4cb283f1800c6a8b3911f936deb392694c40b5d
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2022-09-21 10:54:36 -0500

    feat: un vistaso mal y desactualizado de como crear modulos nativos en c++ con node, se requiere revisar en tal caso mas

---
+++ modulosNativosC++/modulosNativosCmasmas.js
@@ -0,0 +1,3 @@
+//Esto es un tema complejo, pero en pocas palabras debería es poderse ejecutar cosas de c++ en javascript.
+// Se puede investigar como hacer esto de ser requerido más adelante
+// https://www.youtube.com/watch?v=O4Rlq_NSkoM un video que peude servir
