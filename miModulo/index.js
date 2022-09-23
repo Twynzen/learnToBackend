c9b16dd74c6948df46f884b6de79b5ecef97ecca
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2022-09-22 23:35:39 -0500

    fix: eliminando del git unos modulso de pyton nativos

---
+++ miModulo/index.js
@@ -0,0 +1,8 @@
+//Traer un modulo
+const miModulo = require('./modulo')
+
+//Ejecutar la funcion del modulo
+
+console.log(miModulo.prop1);
+miModulo.salute()
+    // miModulo();
---
+++ miModulo/modulo.js
@@ -0,0 +1,8 @@
+function salute() {
+    console.log('Buenas');
+}
+
+module.exports = {
+    salute,
+    prop1: 'Buenas soy una propiedad exportada'
+};
