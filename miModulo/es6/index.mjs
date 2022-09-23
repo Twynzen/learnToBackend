a7fb9466e41230c3c00ffa6f2a343936be6cddcf
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2022-09-22 22:56:25 -0500

    feat: creacion de modulos personalizados

---
+++ miModulo/es6/index.mjs
@@ -0,0 +1,5 @@
+//Importando directamente con ES6
+import miModulo from './modulo.mjs';
+
+console.log(miModulo.prop1);
+miModulo.salute()
---
+++ miModulo/es6/modulo.mjs
@@ -0,0 +1,8 @@
+function salute() {
+    console.log('Buenas');
+}
+
+export default {
+    salute,
+    prop1: 'Buenas soy una propiedad exportada'
+};
